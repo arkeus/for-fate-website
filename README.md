ff24c5657e98483526702413951686ae5b145d05
Lee Miller <iarkeus@gmail.com>
2013-07-21 20:56:41 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Rails is the perfect solution for a single page static website. #trustme
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ForFateWebsite::Application.config.secret_key_base = '214165c62919f6fcd7623c602ef22e5ec724df1125055986cc2a39361ed9b971d3f75996e05b66cc57a8bae44c5fabfa57071cdad7fe5286c5304dbae059a47b'
